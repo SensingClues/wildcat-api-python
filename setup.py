d99f8bd937a99d378f93b67fc1f34b50b7316684
jobvancreij <jobvancrey@hotmail.com>
2022-10-28 12:12:46 +0200

    new wheel

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="wildcatpy",
-    version="0.0.9",
+    version="0.1.0",
     author="sensing_clues",
     author_email="sensingclues@typemail.com",
     description="wildcatpy",
